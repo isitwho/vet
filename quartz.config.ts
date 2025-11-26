4a63a00d960093c4c31d55b12d216070834aa7bd
unknown <isitwho1@gmail.com>
2025-11-27 02:34:32 +0900

    add google analytics

--- quartz.config.ts
+++ quartz.config.ts
@@ -13,7 +13,8 @@ const config: QuartzConfig = {
     enableSPA: true,
     enablePopovers: true,
     analytics: {
-      provider: "plausible",
+      provider: "google",
+      tagId: "G-6GRC86JVBT",
     },
     locale: "ko-KR",
     baseUrl: "isitwho.github.io/vet",
