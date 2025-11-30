8550662fcd08704695886f62d3a37e1874170b61
unknown <isitwho1@gmail.com>
2025-11-30 23:13:48 +0900

    fix list styling

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -17,7 +17,7 @@ export default ((opts?: Options) => {
           {i18n(cfg.locale).components.footer.createdWith}{" "}
           <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
         </p>
-        <ul>
+        <ul style={{ listStyle: "none" }}>
           {Object.entries(links).map(([text, link]) => (
             <li>
               <a href={link}>{text}</a>
