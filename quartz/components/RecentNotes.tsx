b7882ec0914c9f53aac85ce665e60cec3b2c425e
unknown <isitwho1@gmail.com>
2025-11-30 22:25:32 +0900

    edit detail styling

--- quartz/components/RecentNotes.tsx
+++ quartz/components/RecentNotes.tsx
@@ -46,18 +46,18 @@ export default ((userOpts?: Partial<Options>) => {
             return (
               <li class="recent-li">
                 <div class="section">
-                  <div class="desc" style={{ display: "flex", gap: "1rem" }}>
+                  <div class="desc">
                     <h5>
                       <a href={resolveRelative(fileData.slug!, page.slug!)} class="internal">
                         {title}
                       </a>
                     </h5>
-                    {page.dates && (
-                      <p class="meta">
-                        <Date date={getDate(cfg, page)!} locale={cfg.locale} />
-                      </p>
-                    )}
                   </div>
+                  {page.dates && (
+                    <p class="meta">
+                      <Date date={getDate(cfg, page)!} locale={cfg.locale} />
+                    </p>
+                  )}
                   {opts.showTags && (
                     <ul class="tags">
                       {tags.map((tag) => (
